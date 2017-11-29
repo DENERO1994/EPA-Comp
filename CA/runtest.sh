e923d0b3f5664d050a932aba3a248c18f15cfc25
epa <epa@localhost.localdomain>
2017-11-29 13:57:11 +0000

    updates

--- CA/runtest.sh
+++ CA/runtest.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-minNum=1	#variables to store minimum and maximum values 
+minNum=1	#variables to store minimum, maximum values and the duration of the test
 maxNum=50
+duration=5
 
 if [ $1 -lt $minNum ] || [ $1 -gt $maxNum ]; then		#If statement to check that the user input is in the correct range
  echo "parameter is outside of the range - please enter a number between $minNum and $maxNum"
 else
  for ((i = $minNum; i <= $1; i++)); do
-  ./loadtest $i
-  mpstat 5 1
+  ./loadtest $i &
+  mpstat $duration 1 -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'
+  pkill loadtest
  done
 fi
